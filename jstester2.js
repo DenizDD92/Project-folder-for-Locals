bb2b24dc5d9fe5ad0e6c87981e0e19d84d4d634f
DenizDD92 <109040548+DenizDD92@users.noreply.github.com>
2022-07-15 21:26:25 +0300

    commit

--- jstester2.js
+++ jstester2.js
@@ -12,57 +12,57 @@
 
 
 // here we will be putting functions into the objects CAREFUL WITH COMAS AND BRACKETS
-const person=
-{
-    myFunc()
-    {
-        console.log("hi there2");
-    }
-};
+// const person=
+// {
+//     myFunc()
+//     {
+//         console.log("hi there2");
+//     }
+// };
     
-const persona=
-{
-    myfunk2()
-    {
-        console.log("Heyo");
-    }
-};
+// const persona=
+// {
+//     myfunk2()
+//     {
+//         console.log("Heyo");
+//     }
+// };
 
 
-const personas= 
-{
-    myfunk3 ()
-    {
-        console.log('Hey hey Krusty');
-    }
-};
+// const personas= 
+// {
+//     myfunk3 ()
+//     {
+//         console.log('Hey hey Krusty');
+//     }
+// };
 
 
 
 
-const personus=
-{
-    myFunc4()
-    {
-        console.log("Buuuyakesha");
-    }
-};
+// const personus=
+// {
+//     myFunc4()
+//     {
+//         console.log("Buuuyakesha");
+//     }
+// };
 
-let perdonus=
-{
-    myFunc5() 
-    {
-        console.log('Yo yea THATS THE FINISH OF OBJECTS TOPIC');
-    }
+// let perdonus=
+// {
+//     myFunc5() 
+//     {
+//         console.log('Yo yea THATS THE FINISH OF OBJECTS TOPIC');
+//     }
 
-}
+// }
 
 
-person.myFunc();
-persona.myfunk2();
-personas.myfunk3();
-personus.myFunc4();
-perdonus.myFunc5();
+// person.myFunc();
+// persona.myfunk2();
+// personas.myfunk3();
+// personus.myFunc4();
+// perdonus.myFunc5();
 
 
 
@@ -154,16 +154,41 @@ perdonus.myFunc5();
 
 
 
-let i=0;
+// let i=0;
 
-do 
-{
-    console.log(i);
-    i++;
-} while (i<11);
-
-
+// do 
+// {
+//     console.log(i);
+//     i++;
+// } while (i<11);
 
 
 
+
+// STRING VERSION ES5  i ALSO USED "NAMES" COZ "NAME" WAS CROSSED OUT BUT WAS STIULL WORKING(DEPRECIATED_)
+
+// const names = 'John';
+// let age = 29;
+// let profession = 'instructor';
+
+// function personES5() 
+// {
+//     console.log(' my name is '+ names +' I am '+ age + ' years old and I am ' + profession);
+// }
+
+// personES5();
+
+// BOTH ARE SAME
+
+
+// const nameiS = 'John';
+// let age = 29;
+// let profession = 'instructor';
+// SAME ONE WITH ES6 MODIFICATION
+// function personES6() {
+// console.log(`My name is ${nameIs} I am ${age} years old and I am an ${profession} `);
+
+// }
+
+// personES6();
 
---
+++ jsworkout15.7.js
@@ -0,0 +1,79 @@
+// const heading= document.getElementById('heading');
+// console.log(heading);
+// console.log(typeof heading);
+
+// const list = document.getElementById('list');
+// console.log(list.textContent);
+
+// // list.textContent='hi thereee' ;
+
+// console.log(list.innerHTML);
+
+// list.innerHTML='<h1>Yo bro</h1> '
+
+
+// const list = document.querySelector('.list');
+// console.log(list);
+
+// const lis = document.querySelectorAll('ul li');
+// console.log(lis);
+
+//  const listItems= document.querySelectorAll('listItems');
+//  console.log("listItems");
+
+// const heading= document.getElementById('heading');
+// heading.style.backgroundColor='green';
+// heading.style.color='blue';
+
+
+// const lis= document.querySelectorAll("ul li");
+// console.log(lis);
+
+// lis[1].style.backgroundColor="red";
+// lis[0].style.backgroundColor="orange";
+// lis[2].style.backgroundColor="yellow";
+
+
+// const heading = document.querySelector('.heading');
+// const btn = document.querySelector('.btn');
+
+
+// we can also use other things like mouseover mouseout keypress and others
+// btn.onclick= () =>
+// {
+//     console.log('Clicked');
+// };
+
+// btn.onmouseover = () => 
+// {
+//     headiong.style.backgroundColor= "green";
+// };
+// btn.onmouseout = () => 
+// {
+//     headiong.style.backgroundColor= "lightgreen";
+// };
+
+
+// function clickMe()
+// {
+//     console.log("Clicked");
+// }
+
+
+// btn.addEventListener('click', e => {
+//     // heading.style.cssText='background-color: brown; color:white';
+//     console.log(e.target);
+// });
+
+// const paragraph = document.querySelector('.paragraph');
+// console.log(paragraph.getAttribute('id'));
+// console.log(paragraph.getAttribute('class'));
+// console.log(paragraph.getAttribute('title'));
+
+
+// paragraph.setAttribute('style', 'background-color:coral')
+
+// paragraph.removeAttribute('style');
+
+// console.log(paragraph.hasAttribute('style'));
+// console.log(paragraph.hasAttribute('class'));
