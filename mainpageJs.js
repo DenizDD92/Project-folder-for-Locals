995ae0b228b183e949e1be7afe08cbe618a4b3d5
DenizDD92 <109040548+DenizDD92@users.noreply.github.com>
2022-07-23 04:26:44 +0300

    commit

--- mainpageJs.js
+++ mainpageJs.js
@@ -3,14 +3,21 @@
 
 
 // DOM Elements
-
+const btnTop= document.querySelector('.btn-top');   
 
 const mainPage=document.querySelector('.main-page');
 
 const loginPage=document.querySelector('.login-page');
 
-const middleContent=document.querySelector('.middle-content')
+const middleContent=document.querySelector('.middle-content');
 
+const newsFeedPage=document.querySelector(".feeds-page");
+
+const loginModal=document.querySelector('.login-modal');
+
+const modalX=document.querySelector('.login-modal i')
+
+const loginFromBtn = document.querySelector('.login-form-btn')
 // here we are created 2 variables main page and login page. we have to connect them now
 // /*************************************************************************** */
 
@@ -20,6 +27,7 @@ const middleContent=document.querySelector('.middle-content')
 
 const goToLoginPage = () =>     //this is a function
 {
+    
     mainPage.style.display='none';
     loginPage.style.display='grid';
 };
@@ -34,14 +42,49 @@ middleContent.addEventListener('click', e =>
         goToLoginPage();
     }
     
-});
     console.log(e.target.classList[1]) ;
-
-// e.target gives us elements in the console that are actually clicked
-// while e.target.classList will show Log in or Sign in in console as it is on the moment of click. 
+});
 
 
-// experiment // // DESTINATION
+
+
+
+// btnTop.addEventListener('click', e =>
+// {
+    //    if(e.target.classList[0]==='btn-top')
+    //    {
+        //     goToLoginPage();
+        //    }
+    //     console.log(e.target.classList[0]);
+    
+    // });
+    
+    btnTop.addEventListener('click', () => {
+        const inputUserInfo=document.querySelector('.user-info');
+        const inputPassword=document.querySelector('.password');
+        
+        if (inputUserInfo.value !== "" && inputPassword.value !== "" ) 
+        {
+            mainPage.style.display='none';
+            newsFeedPage.style.display='block';
+        } 
+        else 
+        {
+            goToLoginPage();
+            loginModal.style.display='block';
+        }
+        
+        
+        
+        
+    });
+    
+    
+    // e.target gives us elements in the console that are actually clicked
+    // while e.target.classList will show Log in or Sign in in console as it is on the moment of click. 
+    
+    
+    // experiment // // DESTINATION
 // let my_url="https://www.internet-technologies.ru/articles/perenapravlenie-na-druguyu-stranicu-s-pomoschyu-javascript.html";
 
 
@@ -50,3 +93,26 @@ middleContent.addEventListener('click', e =>
 // document.getElementById('sign-up mainbtn').onclick = function () {
 //     window.location.replace(my_url);
 // };
+
+
+// login page 
+modalX.addEventListener('click', () => 
+{
+loginModal.style.display="none"; 
+
+})
+
+loginFromBtn.addEventListener('click', () =>
+{
+    const loginUserInfo=document.querySelector('.login-user-info');
+    const loginPassword=document.querySelector('.login-password');
+    if (loginUserInfo.value !==""&&loginPassword.value!=="") 
+    {
+        loginPage.style.display='none';
+        newsFeedPage.style.display='block';
+    } 
+    else 
+    {
+        loginModal.style.display='block';
+    }
+})
